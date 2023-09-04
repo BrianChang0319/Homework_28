db43a335347bc3e57318bd40980d5af8053cd0a6
Brian Chang <brian199988@yahoo.com.tw>
2023-09-04 16:32:25 +0800

    Push hw1 to GitHub

---
+++ src/hw1/hw1.java
@@ -0,0 +1,49 @@
+package hw1;
+
+public class hw1 {
+	public static void main(String[] args){
+
+	/*	int a = 12, b = 6;
+		System.out.print(a + b + " " + a * b); 
+	*/
+	
+	/*
+		int a = 200 / 12; 
+		b = 200%12;
+		System.out.print(a+"打"+b+"顆");
+	*/
+		
+	/*	int a = 24 , b = 60, c = 60 , d = 256559 ; 
+		//a=1天時數  b=1小時幾分 c=1分幾秒 d=總秒
+		int days = (d/c/b/a);
+		int ls = (d%(a*b*c));
+		int hour = (ls/b/c);
+		int minute = (ls % (b*c))/c;
+		int second = ls % c ;
+		System.out.println(days+"天"+hour+"時"+minute+"分"+second+"秒");
+	*/
+		
+	/*	double a = 3.1415;
+		int b = 5 ;
+		System.out.println("面積"+"="+(b*b*a)+"、"+"周長"+"="+(b*2*a));
+	*/
+		
+	/*	int a = 1500000; // 本金
+		double b = 0.02; // 年利率
+		int c = 10; // 存款年數
+
+		// 進行 10 年的複利計算
+		for (int year = 1; year <= c; year++) {
+			double fee = a * b; // 利息
+			a += fee; // 利息加到本金
+	 }
+		System.out.println(a+ " 元");
+	*/
+		
+	/*	System.out.println(5+5); //正常加法
+		System.out.println(5+'5'); // '5'是unicode 53  然後相加
+		System.out.println(5+"5"); //5+字元5 數字結合
+	*/
+	 }
+
+}
